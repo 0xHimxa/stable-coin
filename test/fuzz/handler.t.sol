cca1d4aaa8ac29d602315c5a506cfc1408ca704c
himxa <himxa0x@gmail.com>
2025-12-16 10:43:37 +0100

    write invariant test

---
+++ test/fuzz/handler.t.sol
@@ -0,0 +1,3 @@
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20; //is goona handlethe way we call function so we dont waste runs
--- test/fuzz/invariantsTest.t.sol
+++ test/fuzz/invariantsTest.t.sol
@@ -1,1 +1,39 @@
 //Have our invariant aka properties that should always hold
+
+//  what are our invariants
+
+// 1. The total suplly of DSC should be less than the totla value of collateral
+
+
+// 2. Getter view function should never revert <-- evergreen invariant
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {DeployDSC} from "script/DeployDSC.s.sol";
+
+import {Test, console} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DecentralisedStableCoin} from "src/Decentrialsed.s.sol";
+import {DSCEngine} from "src/DSCEngine.sol";
+import {HelperConfig} from "script/helperConfig.s.sol";
+
+contract InvariantsTest is  StdInvariant, Test{
+
+    DeployDSC deployer;
+DSCEngine dscengine;
+DecentralisedStableCoin dsc;
+HelperConfig.NetworkConfig config;
+
+    function setUp() external {
+        deployer = new DeployDSC();
+        (dscengine, dsc, config) = deployer.run();
+
+
+    }
+
+
+   
+
+
+}
